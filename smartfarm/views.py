148322c7b773401ef5dba4163ff61d07f941976b
PanupunJanin <panupan.ja@gmail.com>
2025-04-22 21:47:44 +0700

    Add Presentation files with score calculator in detail page

--- smartfarm/views.py
+++ smartfarm/views.py
@@ -35,7 +35,76 @@ class CropListView(ListView):
 class CropDetailView(DetailView):
     model = Crop
     template_name = 'crops/detail.html'
-    context_object_name = 'crop'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        crop = self.get_object()
+
+        # Get input values from GET parameters
+        temp = self.request.GET.get('temperature')
+        humidity = self.request.GET.get('humidity')
+        light = self.request.GET.get('light')
+        wet = self.request.GET.get('wet') == 'true'
+
+        if temp and humidity and light:
+            temperature = float(temp)
+            humidity = float(humidity)
+            light = float(light)
+
+            # Temperature score
+            temp_range = crop.optimal_temperature_max - crop.optimal_temperature_min
+            if temp_range > 0:
+                temp_score = max(0, 1 - abs(temperature - ((crop.optimal_temperature_min + crop.optimal_temperature_max) / 2)) / (temp_range / 2))
+            else:
+                temp_score = 1 if temperature == crop.optimal_temperature_min else 0
+
+            # Humidity score
+            humidity_range = crop.optimal_humidity_max - crop.optimal_humidity_min
+            if humidity_range > 0:
+                humidity_score = max(0, 1 - abs(humidity - ((crop.optimal_humidity_min + crop.optimal_humidity_max) / 2)) / (humidity_range / 2))
+            else:
+                humidity_score = 1 if humidity == crop.optimal_humidity_min else 0
+
+            # Light score
+            if crop.optimal_light_min <= light <= crop.optimal_light_max:
+                light_score = 1.0
+            else:
+                light_range = crop.optimal_light_max - crop.optimal_light_min
+                if light < crop.optimal_light_min:
+                    dist = crop.optimal_light_min - light
+                else:
+                    dist = light - crop.optimal_light_max
+                light_score = max(0, 1 - dist / (light_range / 2)) if light_range > 0 else 0
+
+            # Overall score (percentage)
+            overall_score = round((temp_score + humidity_score + light_score) / 3 * 100, 2)
+
+            context.update({
+                'temperature': temperature,
+                'humidity': humidity,
+                'light': light,
+                'wet': wet,
+                'temp_score': round(temp_score * 100, 2),
+                'humidity_score': round(humidity_score * 100, 2),
+                'light_score': round(light_score * 100, 2),
+                'overall_score': overall_score,
+                'suggestions': {
+                    'temp': {
+                        'delta': temperature - (crop.optimal_temperature_min + crop.optimal_temperature_max) / 2,
+                        'optimal': (crop.optimal_temperature_min + crop.optimal_temperature_max) / 2
+                    },
+                    'humidity': {
+                        'delta': humidity - (crop.optimal_humidity_min + crop.optimal_humidity_max) / 2,
+                        'optimal': (crop.optimal_humidity_min + crop.optimal_humidity_max) / 2
+                    },
+                    'light': {
+                        'delta': light - (crop.optimal_light_min + crop.optimal_light_max) / 2,
+                        'optimal': (crop.optimal_light_min + crop.optimal_light_max) / 2
+                    }
+                }
+            })
+
+        return context
 
 
 def get_crop_data(request):
